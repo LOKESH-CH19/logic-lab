7c37d8ac2d249677a86fc1a698f25c1135b713f5
LOKESH CH <lokesh.ch118@gmail.com>
2025-08-15 21:58:43 +0530

    Update LC-2561.py

--- Arrays/LC-2561.py
+++ Arrays/LC-2561.py
@@ -1,3 +1,26 @@
+"""
+Problem : Rearranging Fruits
+Link : https://leetcode.com/problems/rearranging-fruits/
+Description : You have two fruit baskets containing n fruits each. You are given two 0-indexed integer arrays basket1 and basket2 representing the cost of fruit in each basket. You want to make both baskets equal. To do so, you can use the following operation as many times as you want:
+    Choose two indices i and j, and swap the ith fruit of basket1 with the jth fruit of basket2.
+    The cost of the swap is min(basket1[i], basket2[j]).
+    Two baskets are considered equal if sorting them according to the fruit cost makes them exactly the same baskets.
+    Return the minimum cost to make both the baskets equal or -1 if impossible.
+
+Approach : 
+        I take a hash table and count the fruits from basket1 and from same hash table decrease the count of fruits if it also present in basket2.
+        That remains me the fruits and their count which are only available in basket1 and basket2(-ve count).
+        if count of any fruit type is odd that means it is impossible.
+        initilise 2 arrays one for extra fruit type that need to swapped from basket1 and second array for basket2.
+        sort both arrays one in asc and one in des at each index find the min element at that point use that value for min cost
+        if the lowest element is still considered large we can alter by
+        find the lowest element from both arrays use as a temoporary element to reduce the cost as swap the min element twice , one for swap basket1 with min and next min with basket2 fruit type
+        find which gives the lowest value for that instant either double swap of lowest element or min element at that instant.
+        summate all the values until array become empty(i.e both baskets will be identical)
+        
+Time complexity: O(nlogn)
+Space complexity: O(n)
+"""
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
         set1=set(basket1)
