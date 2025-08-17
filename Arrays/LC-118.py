0bead708f6a846243628a0a67122c5f6b7fb2e2d
LOKESH CH <lokesh.ch118@gmail.com>
2025-08-17 21:41:04 +0530

    Update LC-118.py

--- Arrays/LC-118.py
+++ Arrays/LC-118.py
@@ -1,3 +1,24 @@
+"""
+Problem: Pascal Triangle
+Link: https://leetcode.com/problems/pascals-triangle/description/
+Difficulty: Easy
+
+Description:
+            Given an integer numRows, return the first numRows of Pascal's triangle.
+            In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+                                       1
+                                      1 1
+                                     1 2 1
+                                    1 3 3 1
+Approach:
+        
+
+Time Complexity: O(n^2)
+Space Complexity: O(n^2)
+"""
+
+
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         arr=[]
